fc4999d346ceb5f68dccd29c695eb5c74e43bbde
Leah Seyoum <leahseyoum@gmail.com>
2023-02-23 16:29:27 -0800

    moves

---
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/towers_of_hanoi.rb
@@ -0,0 +1,38 @@
+
+class Towersofhanoi
+    attr_reader :stack
+
+    def initialize
+        @stack = [
+        [4, 3, 2, 1],
+        [], 
+        []
+    ]
+    end
+
+    def valid?(input)
+        v1, v2 = input
+        @stack[v1].last < @stack[v2]
+    end
+
+    def moves
+        p @stack
+        puts "Input should look like two numbers with a space"
+        res = gets.chomp.split(" ").map(&:to_i)
+        if valid?(res)
+            v1, v2 = res
+            @stacks[v2] << @stacks[v1].last
+        end
+        
+        p @stack
+    end
+
+    def won
+        if self[2] == [4, 3, 2, 1]
+            puts "YOU WON!"
+            return true
+        else
+            return false
+        end
+    end
+end
